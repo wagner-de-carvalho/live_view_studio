8d336d7f1b226ee4ac53213fe1d11c84f6677b6c
mindlab <wagncarv@outlook.com>
2023-11-04 16:30:54 -0300

    Habilitar debug socket

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -9,20 +9,46 @@ defmodule LiveViewStudioWeb.LightLive do
 
   def render(assigns) do
     ~H"""
-    <h1 class="font-bold text-center text-4xl mb-10">Front Porch Light</h1>
+    <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
         <span style={"width: #{@brightness}%"}>
           <%= @brightness %>%
         </span>
       </div>
-      <button>
+      <button phx-click="off">
         <img src="/images/light-off.svg" alt="Light off" />
       </button>
-      <button>
+      <button phx-click="down">
+        <img src="/images/down.svg" alt="Down" />
+      </button>
+      <button phx-click="up">
+        <img src="/images/up.svg" alt="Up" />
+      </button>
+      <button phx-click="on">
         <img src="/images/light-on.svg" alt="Light on" />
       </button>
     </div>
     """
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, brightness: 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket = update(socket, :brightness, &(&1 + 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket = update(socket, :brightness, &(&1 - 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, brightness: 0)
+    {:noreply, socket}
   end
 end
